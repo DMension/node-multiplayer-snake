7152c8f5b8ec9be53758bb0af5a8497afe26bb0b
amritsql <amritsql@gmail.com>
2019-09-04 23:35:40 +0800

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,6 +1,6 @@
 const sonarqubeScanner = require('sonarqube-scanner');
      sonarqubeScanner({
-       serverUrl: 'http://localhost:9000',
+       serverUrl: 'http://192.168.56.102:9000',
        options : {
        'sonar.sources': '.',
        'sonar.inclusions' : './**' // Entry point of your code
