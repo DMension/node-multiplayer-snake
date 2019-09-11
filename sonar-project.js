e2fb1e6616c17225348801c48624275938f8a23d
amritsql <amritsql@gmail.com>
2019-09-11 22:38:41 +0800

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -3,6 +3,6 @@ const sonarqubeScanner = require('sonarqube-scanner');
        serverUrl: 'http://192.168.56.102:9000',
        options : {
        'sonar.sources': '.',
-       'sonar.inclusions' : '.' // Entry point of your code
+       //'sonar.inclusions' : '.' // Entry point of your code
        }
      }, () => {});
