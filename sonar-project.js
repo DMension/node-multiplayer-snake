a7a605141bd42ae04b8b01b40e45eb361287aa2d
jenkins <trainmefordevsecops@gmail.com>
2019-09-18 12:13:13 +0800

    initial commit for repository

--- sonar-project.js
+++
@@ -1,8 +0,0 @@
-const sonarqubeScanner = require('sonarqube-scanner');
-     sonarqubeScanner({
-       serverUrl: 'http://192.168.56.102:9000',
-       options : {
-       'sonar.sources': '.',
-       //'sonar.inclusions' : '.' // Entry point of your code
-       }
-     }, () => {});
